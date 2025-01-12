e444c41cac30b43389053047d61e9f414709d0be
Ghanindra <bohoraghanindra@gmail.com>
2025-01-12 21:52:48 +0545

    added register and login

--- Models/User.cs
+++ Models/User.cs
@@ -10,16 +10,20 @@ namespace Coursework.Models
 
     {
 
-        public int Id { get; set; } // Unique identifier for the user
+        //public int Id { get; set; } // Unique identifier for the user
 
-        public string Name { get; set; } // Name of the user
+        //public string Name { get; set; } // Name of the user
 
-        public string Email { get; set; } // Email address
+        //public string Email { get; set; } // Email address
 
-        public string Phone { get; set; } // Contact number
+        //public string Phone { get; set; } // Contact number
 
-        public string Address { get; set; } // Address (optional)
+        //public string Address { get; set; } // Address (optional)
+        //public string Password { get; set; } // This will store the hashed password
 
+        public string Username { get; set; }  // This is required for the registration logic
+        public string Password { get; set; }  // This will store the hashed password
+        public string Email { get; set; }
         public DateTime CreatedOn { get; set; } = DateTime.Now; // Date the user was added
 
     }
